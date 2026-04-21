98f992fedb0eaf51cec80da634f3ccf2c1886d8f
함동현 <haamdonghyun0918@gmail.com>
2026-04-21 16:03:18 +0900

    순수 C# 코드까지 참조완료

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Car
+{
+    public string CarName { get; set; }
+    public void Clarkson()
+    {
+        Debug.LogWarning("차량이 클라션을 울린다: 빵빵");
+    }
+}
--- Assets/Scripts/CarBehaviourScript.cs
+++ Assets/Scripts/CarBehaviourScript.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class CarBehaviourScript : MonoBehaviour
 {
+    private Car carCode = new Car();
     private void Awake()
     {
+        carCode.Clarkson();
         Debug.Log("이래 보여도 자동차 입니다.");
     }
 }
