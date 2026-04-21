ca50ca77327d44c261f11d625a22b6b7f0e59f8f
함동현 <haamdonghyun0918@gmail.com>
2026-04-21 16:19:20 +0900

    모노비헤이비어의 라이프 사이클 마무리

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -7,4 +7,16 @@ public class Car
     {
         Debug.LogWarning("차량이 클라션을 울린다: 빵빵");
     }
+    public void Move()
+    {
+        Debug.Log("차량이 움직입니다: 부릉부릉");
+    }
+    public void Cancel()
+    {
+        Debug.LogWarning("차량 상태가 매우 좋지 않아 이 차량의 계약을 잠시 해지해야 합니다. 그동안 다른 차량을 타야겠네요...");
+    }
+    public void BreakCar()
+    {
+        Debug.LogWarning("아마 이 차량은 폐차장으로 갈 것 같습니다.. 잘 있어라 차량아...");
+    }
 }
--- Assets/Scripts/CarBehaviourScript.cs
+++ Assets/Scripts/CarBehaviourScript.cs
@@ -3,9 +3,58 @@
 public class CarBehaviourScript : MonoBehaviour
 {
     private Car carCode = new Car();
+    bool _isFirstFixedUpdate = false;
+    bool _isFirstUpdate = false;
+    bool _isFirstLateUpdate = false;
     private void Awake()
     {
-        carCode.Clarkson();
         Debug.Log("이래 보여도 자동차 입니다.");
+    }
+    private void OnEnable()
+    {
+        carCode.Clarkson();
+    }
+    private void Start()
+    {
+        carCode.Move();
+    }
+
+    private void FixedUpdate()
+    {
+        if(_isFirstFixedUpdate == true)
+        {
+            return;
+        }
+
+        _isFirstFixedUpdate = true;
+        Debug.Log("차량에서 연기가 납니다.. 내려서 조치를 취해야겠습니다.");
+    }
+    private void Update()
+    {
+        if(_isFirstUpdate == true)
+        {
+            return;
+        }
+
+        _isFirstUpdate = true;
+        Debug.Log("큰 문제는 아니고 과열이 심하게 된 것 같습니다.. 연료도 부족하고요..");
+    }
+    private void LateUpdate()
+    {
+        if(_isFirstLateUpdate == true)
+        {
+            return;
+        }
+
+        _isFirstLateUpdate = true;
+        Debug.Log("보험사를 불러 빠르게 해결하려 했는데, 뭔가 이상합니다.");
+    }
+    private void OnDisable()
+    {
+        carCode.Cancel();
+    }
+    private void OnDestroy()
+    {
+        carCode.BreakCar();
     }
 }
