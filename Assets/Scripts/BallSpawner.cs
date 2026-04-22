c1bf777d5d9b686f7531985a2e929d430a190758
함동현 <haamdonghyun0918@gmail.com>
2026-04-22 15:37:24 +0900

    Ball 컴포넌트와 객체를 생성 후, 스폰장소에 지정하여 생성

---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    public GameObject Prefab_Ball;
+
+    private void OnEnable()
+    {
+        // 1) 프리팹의 게임오브젝트 동적 생성 => new / Heap메모리
+        Instantiate(Prefab_Ball);
+    }
+}
