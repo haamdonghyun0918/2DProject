08f46fc4d410da5126350d54504b0719121687c5
함동현 <haamdonghyun0918@gmail.com>
2026-04-22 16:08:58 +0900

    Update문에서 KeyCode를 통하여 Prefab된 물체 생성완료

--- Assets/Scripts/BallSpawner.cs
+++ Assets/Scripts/BallSpawner.cs
@@ -4,7 +4,16 @@ public class BallSpawner : MonoBehaviour
 {
     public GameObject Prefab_Ball;
 
-    private void OnEnable()
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SpwanPrefab();
+        }
+
+    }
+
+    private void SpwanPrefab()
     {
         // 1) 프리팹의 게임오브젝트 동적 생성 => new / Heap메모리
         Instantiate(Prefab_Ball);
