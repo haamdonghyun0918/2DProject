317e501c4f76bdeaaedda0205a6d8222583962f2
함동현 <haamdonghyun0918@gmail.com>
2026-04-23 17:09:15 +0900

    Q,E로 회전하는 기능 추가

--- Assets/Scripts/PlayerMoving.cs
+++ Assets/Scripts/PlayerMoving.cs
@@ -2,32 +2,54 @@
 
 public class PlayerMoving : MonoBehaviour
 {
-    float moveStep = 0.5f;
+    bool isStartMove = false;
+    Vector3 moveDirection;
+    float rotateValue = 0.0f;
     private void OnEnable()
     {
         Debug.Log("플레이어입니다. 절대 박스가 아닙니다. 플레이어입니다!");
     }
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W))
+        if (isStartMove == false)
+        {
+            moveDirection = Vector3.zero;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
         {
             Debug.Log("위로 움직입니다");
-            transform.Translate(Vector3.forward * moveStep);
+            isStartMove = true;
+            moveDirection = Vector3.forward;
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
             Debug.Log("아래로 움직입니다");
-            transform.Translate(Vector3.back * moveStep);
+            isStartMove = true;
+            moveDirection = Vector3.back;
         }
         else if (Input.GetKeyDown(KeyCode.A))
         {
             Debug.Log("뒤로 움직입니다");
-            transform.Translate(Vector3.left * moveStep);
+            isStartMove = true;
+            moveDirection = Vector3.left;
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
             Debug.Log("앞으로 움직입니다");
-            transform.Translate(Vector3.right * moveStep);
+            isStartMove = true;
+            moveDirection = Vector3.right;
+        }
+        else if(Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log("왼쪽으로 회전");
+            rotateValue = rotateValue - 45.0f;
+            this.gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, rotateValue, 0));
+        }
+        else if(Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("오른쪽으로 회전");
+            rotateValue = rotateValue + 45.0f;
+            this.gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, rotateValue, 0));
         }
 
         else if (Input.GetKeyDown(KeyCode.Space))
@@ -39,5 +61,6 @@ public class PlayerMoving : MonoBehaviour
                 rigidBody.AddForce(Vector3.up * 200.0f);
             }
         }
+        this.gameObject.transform.Translate(moveDirection * Time.deltaTime);
     }
 }
