2bdcf7636277ee45004c53aa55edc54d559b3a72
함동현 <haamdonghyun0918@gmail.com>
2026-04-23 16:45:45 +0900

    wasd움직임인데, 순간이동하는 듯한 움직임

--- Assets/Scripts/PlayerMoving.cs
+++ Assets/Scripts/PlayerMoving.cs
@@ -2,42 +2,42 @@
 
 public class PlayerMoving : MonoBehaviour
 {
-    bool isStartMove = false;
-    Vector3 moveDirection;
+    float moveStep = 0.5f;
     private void OnEnable()
     {
         Debug.Log("플레이어입니다. 절대 박스가 아닙니다. 플레이어입니다!");
     }
     private void Update()
     {
-        if (isStartMove == false)
-        {
-            moveDirection = Vector3.zero;
-        }
         if(Input.GetKeyDown(KeyCode.W))
         {
             Debug.Log("위로 움직입니다");
-            isStartMove = true;
-            moveDirection = Vector3.forward;
+            transform.Translate(Vector3.forward * moveStep);
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
             Debug.Log("아래로 움직입니다");
-            isStartMove = true;
-            moveDirection = Vector3.back;
+            transform.Translate(Vector3.back * moveStep);
         }
         else if (Input.GetKeyDown(KeyCode.A))
         {
             Debug.Log("뒤로 움직입니다");
-            isStartMove = true;
-            moveDirection = Vector3.left;
+            transform.Translate(Vector3.left * moveStep);
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
             Debug.Log("앞으로 움직입니다");
-            isStartMove = true;
-            moveDirection = Vector3.right;
+            transform.Translate(Vector3.right * moveStep);
         }
-        this.gameObject.transform.Translate(moveDirection * Time.deltaTime);
+
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var rigidBody = this.gameObject.GetComponent<Rigidbody>();
+            if (rigidBody != null)
+            {
+                Debug.Log("점프하고 있습니다.");
+                rigidBody.AddForce(Vector3.up * 200.0f);
+            }
+        }
     }
 }
