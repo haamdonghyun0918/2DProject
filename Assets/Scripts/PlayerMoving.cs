c8e98e50f64d9d0ba133fa4768dc335c9e6b0351
함동현 <haamdonghyun0918@gmail.com>
2026-04-23 16:16:42 +0900

    wasd 움직임 수정본

--- Assets/Scripts/PlayerMoving.cs
+++ Assets/Scripts/PlayerMoving.cs
@@ -4,10 +4,9 @@ public class PlayerMoving : MonoBehaviour
 {
     bool isStartMove = false;
     Vector3 moveDirection;
-    float rotateValue;
     private void OnEnable()
     {
-        Debug.Log("플레이어가 나타났습니다! 절대 공 아닙니다. 플레이어 입니다!");
+        Debug.Log("플레이어입니다. 절대 박스가 아닙니다. 플레이어입니다!");
     }
     private void Update()
     {
@@ -17,34 +16,28 @@ public class PlayerMoving : MonoBehaviour
         }
         if(Input.GetKeyDown(KeyCode.W))
         {
+            Debug.Log("위로 움직입니다");
             isStartMove = true;
             moveDirection = Vector3.forward;
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
+            Debug.Log("아래로 움직입니다");
             isStartMove = true;
             moveDirection = Vector3.back;
         }
         else if (Input.GetKeyDown(KeyCode.A))
         {
+            Debug.Log("뒤로 움직입니다");
             isStartMove = true;
             moveDirection = Vector3.left;
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
+            Debug.Log("앞으로 움직입니다");
             isStartMove = true;
             moveDirection = Vector3.right;
         }
-        //else if (Input.GetKeyDown(KeyCode.Q))
-        //{
-        //    this.gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, -45.0f, 0));
-        //}
-        //else if (Input.GetKeyDown(KeyCode.E))
-        //{
-        //    this.gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 45.0f, 0));
-        //}
-        //rotateValue = (rotateValue + 45.0f);
-        //this.gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, rotateValue, 0));
         this.gameObject.transform.Translate(moveDirection * Time.deltaTime);
     }
 }
