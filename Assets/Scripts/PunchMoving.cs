f67c58ebfc435e6ae4047bb67822fdc2dae50e51
함동현 <haamdonghyun0918@gmail.com>
2026-05-14 18:32:44 +0900

    [project] 주인공 애니메이션 제작 완료

---
+++ Assets/Scripts/PunchMoving.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class PunchMoving : MonoBehaviour
+{
+    [SerializeField] private Animator Punch_Entity;
+
+    [SerializeField] private float maxRunSpeed = 10.0f;
+    [SerializeField] private float acceleration = 2.0f;
+    [SerializeField] private float deceleration = 10.0f;
+
+    private float currentSpeed = 0.0f;
+
+    private void Update()
+    {
+        Move();
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Punch_Entity.SetTrigger("isAttack");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Punch_Entity.SetTrigger("isJump");
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.Rotate(0, 180f, 0);
+        }
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            currentSpeed += acceleration * Time.deltaTime;
+
+            if (currentSpeed > maxRunSpeed)
+            {
+                currentSpeed = maxRunSpeed;
+            }
+        }
+        else
+        {
+            currentSpeed -= deceleration * Time.deltaTime;
+            if (currentSpeed < 0)
+            {
+                currentSpeed = 0;
+            }
+        }
+        Vector2 moveDir = new Vector2(1, 0);
+        transform.Translate(moveDir * currentSpeed * Time.deltaTime);
+
+        Punch_Entity.SetFloat("Speed", currentSpeed);
+    }
+}
