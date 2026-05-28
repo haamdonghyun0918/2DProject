60825a57a7406f244f420cfa1d1b801be2b68e3c
함동현 <haamdonghyun0918@gmail.com>
2026-05-28 16:41:35 +0900

    [2D Game Project] Clear화면에서 보상 선택 추가 하는 중...

--- Assets/Scripts/GameProject_1/ClearPopUp.cs
+++ Assets/Scripts/GameProject_1/ClearPopUp.cs
@@ -3,15 +3,26 @@
 public class ClearPopUp : UiBase
 {
     [SerializeField] private UiButton button_Exit;
-
+    [SerializeField] private UiButton button_Card;
+    [SerializeField] private UiButton button_Rest;
     public void OnEnable()
     {
         button_Exit.BindOnClickButtonEvent(OnClickPopUpExit);
+        button_Card.BindOnClickButtonEvent(OnClickReward);
+        button_Rest.BindOnClickButtonEvent(OnClickHeal);
     }
     public void OnClickPopUpExit()
     {
         UiManager.Instance.OpenGameMainScene();
         UiManager.Instance.CloseStageUi();
         UiManager.Instance.CloseClearPopUp();
+    }
+    public void OnClickReward()
+    {
+
+    }
+    public void OnClickHeal()
+    {
+
     }
 }
